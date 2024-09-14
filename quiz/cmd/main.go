64a333ec5188bdca66310e625cf1230522efe5bb
Javed <jaybee.codes@gmail.com>
2024-09-14 17:19:07 +0300

    feat: add ReadCsvFile function

--- quiz/cmd/main.go
+++ quiz/cmd/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/JayBee007/gophercises/quiz/pkg/csv"
 )
 
 func main() {
-	csv.Log()
+	records := csv.ReadCsvFile("internal/problems.csv")
+
+	for index, outer := range records {
+		question, answer := outer[0], outer[1]
+
+		fmt.Println("No.", index, "Question:", question, "Answer:", answer)
+	}
 }
--- quiz/pkg/csv/csv.go
+++ quiz/pkg/csv/csv.go
@@ -1,7 +1,27 @@
 package csv
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
 
-func Log() {
-	fmt.Println("Log from csv package")
+func ReadCsvFile(filePath string) [][]string {
+	file, err := os.Open(filePath)
+
+	if err != nil {
+		fmt.Println("Unable to read input file "+filePath, err)
+	}
+
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+
+	records, err := csvReader.ReadAll()
+
+	if err != nil {
+		fmt.Println("Unable to parse file as CSV for "+filePath, err)
+	}
+
+	return records
 }
